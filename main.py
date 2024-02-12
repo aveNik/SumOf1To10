1e45c25302140ec610f79a215c58548dbececf28
aveNik <158022654+aveNik@users.noreply.github.com>
2024-02-12 09:22:27 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#wellcome
+print("maded by aveNik")
+print("=-=-=-=-=-=-=-=")
+print("")
+
+#key variable
+s=0
+
+#main  part
+for i in range(1, 11):
+    s=s+i
+    print("now summ №", i, "and equals", s)
+
+print("Summ of the numbers from 1 to 100 is", s)
